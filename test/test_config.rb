661f0ace8127f9a36caad2bd087758efe3f8cf8a
Pyer <pierre.bazonnard@gmail.com>
2016-07-01 11:29:16 +0200

    * change user and database names for Travis testing

--- test/test_config.rb
+++ test/test_config.rb
@@ -3,15 +3,15 @@ require 'minitest/autorun'
 require './lib/config.rb'
 
 class TestConfig < Minitest::Test
-  def test_config_initialize
+  def test_config_test_env
     ENV['env'] = 'test'
     conf = Config.new
     assert_equal(conf.env,      'test')
     assert_equal(conf.host,     'localhost')
     assert_equal(conf.port,     '5432')
-    assert_equal(conf.username, 'pba')
-    assert_equal(conf.password, 'pba')
-    assert_equal(conf.database, 'pyer_test')
+    assert_equal(conf.username, 'postgres')
+    assert_equal(conf.password, '')
+    assert_equal(conf.database, 'travis_ci_test')
     assert_equal(conf.version0, '0.00')
   end
 
